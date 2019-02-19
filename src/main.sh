3b6835b27f634e76947b52c35e3f8a1365583976
Nelson Martell <nelson6e65@gmail.com>
2019-02-19 07:06:42 -0400

    :up: :memo: Improve reading of .env config

--- src/main.sh
+++ src/main.sh
@@ -5,35 +5,75 @@
 declare SCRIPT_DIR=
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ -f .env ]]; then
-    # shellcheck disable=SC1091
-    . .env
-else
-    echo 'Error: No ".env" file found'
-    exit 1
+# Variables de configuración
+declare -x c_host
+declare -ix c_port
+declare -x c_database
+declare -x c_username
+declare -x c_password
+
+function f_import_env()
+{
+    declare -i errors=0
+
+    if [[ -f .env ]]; then
+        # shellcheck disable=SC1091
+        . .env
+        errors=$?
+    else
+        echo 'Error: No ".env" file found'
+        errors=0
+    fi
+
+    if [[ $errors -eq 0 ]]; then
+        if [[ -z $DB_HOST ]]; then
+            declare DB_HOST='localhost'
+        fi
+
+        if [[ -z $DB_PORT ]]; then
+            declare DB_PORT=3306
+        fi
+
+        if [[ -z $DB_DATABASE ]]; then
+            if [[ -z $DB_NAME ]]; then
+                echo 'Error: No se encontró "DB_NAME" o "DB_DATABASE"'
+                errors=$(( errors | 2 ))
+            else
+                declare DB_DATABASE="$DB_NAME"
+            fi
+        fi
+
+        if [[ -z $DB_USERNAME ]]; then
+            echo 'Error: No se encontró "DB_USERNAME"'
+            errors=$(( errors | 2 ))
+        fi
+
+        if [[ -z $DB_PASSWORD ]]; then
+            echo 'Error: No se encontró "DB_PASSWORD"'
+            errors=$(( errors | 2 ))
+        fi
+
+        if [[ $errors -eq 0 ]]; then
+            c_host=$DB_HOST
+            c_port=$DB_PORT
+            c_database=$DB_DATABASE
+            c_username=$DB_USERNAME
+            c_password=$DB_PASSWORD
+        fi
+    fi
+
+    return $errors
+}
+
+f_import_env
+r=$?
+
+if [[ $r -ne 0   ]]; then
+    echo 'Ocurrió un error al obtener los credenciales de la base de datos'
+    exit $r
 fi
 
-
-if [[ -z $DB_HOST ]]; then
-    declare DB_HOST='localhost'
-fi
-
-if [[ -z $DB_NAME ]]; then
-    echo 'Error: "DB_NAME" no está definido'
-    exit 1
-fi
-
-if [[ -z $DB_USERNAME ]]; then
-    echo 'Error: "DB_USERNAME" no está definido'
-    exit 1
-fi
-
-if [[ -z $DB_PASSWORD ]]; then
-    echo 'Error: "DB_PASSWORD" no está definido'
-    exit 1
-fi
-
-declare file=
+declare t_file=
 
 declare -i r=0
 declare -i opt_b=0 # Respaldo (por defecto si no se pasa --no-backup)
@@ -49,9 +89,9 @@ function f_backup
 
     hora=$(date +%Y-%m-%d_%H.%M.%S)
 
-    file="backups/${DB_NAME}_${hora}.sql.gz"
+    t_file="backups/${c_database}_${hora}.sql.gz"
 
-    mysqldump -v --opt --events --routines --triggers --default-character-set=utf8mb4 -h "${DB_HOST}" -u "${DB_USERNAME}" --password="${DB_PASSWORD}" "${DB_NAME}" | gzip -c > "${file}"
+    mysqldump -v --opt --events --routines --triggers --default-character-set=utf8mb4 -h "${c_host}" -u "${c_username}" --password="${c_password}" "${c_database}" | gzip -c > "${t_file}"
 
     return $?
 }
@@ -59,9 +99,9 @@ function f_backup
 # Reemplaza el SQL actual
 function f_export
 {
-    file="${DB_NAME}.sql"
+    t_file="${c_database}.sql"
 
-    mysqldump -v --opt --events --routines --triggers --default-character-set=utf8mb4 -h "${DB_HOST}" -u "${DB_USERNAME}" --password="${DB_PASSWORD}" "${DB_NAME}" > "${file}"
+    mysqldump -v --opt --events --routines --triggers --default-character-set=utf8mb4 -h "${c_host}" -u "${c_username}" --password="${c_password}" "${c_database}" > "${t_file}"
 
     return $?
 }
@@ -70,9 +110,9 @@ function f_export
 
 function f_import
 {
-    file="${DB_NAME}.sql"
+    t_file="${c_database}.sql"
 
-    mysql -h "${DB_HOST}" -u "${DB_USERNAME}" --password="${DB_PASSWORD}" "${DB_NAME}" < "${file}"
+    mysql -h "${c_host}" -u "${c_username}" --password="${c_password}" "${c_database}" < "${t_file}"
 
     return $?
 }
@@ -110,31 +150,31 @@ done
 
 r=0
 if [[ $opt_b -eq 0 ]]; then
-    echo "Respaldando Base de Datos '${DB_NAME}'..."
+    echo "Respaldando Base de Datos '${c_database}'..."
 
     f_backup
     r=$?
 
     echo
     if [[ $r -eq 0 ]]; then
-        echo "  Respaldo creado: ${file}."
+        echo "  Respaldo creado: ${t_file}."
     else
         echo '  Error: No se pudo crear el respaldo'
-        rm "${file}"
+        rm "${t_file}"
     fi
     echo
 fi
 
 if [[ $r -eq 0 ]]; then
     if [[ $opt_e -eq 0 ]]; then
-        echo "Exportando Base de Datos '${DB_NAME}'..."
+        echo "Exportando Base de Datos '${c_database}'..."
 
         f_export
         r=$?
 
         echo
         if [[ $r -eq 0 ]]; then
-            echo "  Exportación exitosa: '${file}'."
+            echo "  Exportación exitosa: '${t_file}'."
         else
             echo '  Error: No se pudo exportar la base de datos'
         fi
@@ -144,14 +184,14 @@ fi
 
 if [[ $r -eq 0 ]]; then
     if [[ $opt_i -eq 0 ]]; then
-        echo "Importando Base de Datos '${DB_NAME}'..."
+        echo "Importando Base de Datos '${c_database}'..."
 
         f_import
         r=$?
 
         echo
         if [[ $r -eq 0 ]]; then
-            echo "  Importanción exitosa desde '${file}'."
+            echo "  Importanción exitosa desde '${t_file}'."
         else
             echo '  Error: No se pudo importar la base de datos'
         fi
